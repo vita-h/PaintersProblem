99aac9c77ed31099130b2ed1779b787e75f33272
Vita <Vita@DESKTOP-11369FC>
2018-01-15 22:05:20 +0100

    Add project files.

---
+++ AA_Detyra1_v2/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace AA_Detyra1_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.nupNrPainters = new System.Windows.Forms.NumericUpDown();
+            this.lblNrPainters = new System.Windows.Forms.Label();
+            this.rtbRez = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nupNrPainters)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nupNrPainters
+            // 
+            this.nupNrPainters.Location = new System.Drawing.Point(15, 29);
+            this.nupNrPainters.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nupNrPainters.Name = "nupNrPainters";
+            this.nupNrPainters.Size = new System.Drawing.Size(95, 22);
+            this.nupNrPainters.TabIndex = 3;
+            this.nupNrPainters.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lblNrPainters
+            // 
+            this.lblNrPainters.AutoSize = true;
+            this.lblNrPainters.Location = new System.Drawing.Point(12, 9);
+            this.lblNrPainters.Name = "lblNrPainters";
+            this.lblNrPainters.Size = new System.Drawing.Size(98, 17);
+            this.lblNrPainters.TabIndex = 1;
+            this.lblNrPainters.Text = "Nr. of painters";
+            // 
+            // rtbRez
+            // 
+            this.rtbRez.Location = new System.Drawing.Point(15, 66);
+            this.rtbRez.Name = "rtbRez";
+            this.rtbRez.Size = new System.Drawing.Size(498, 320);
+            this.rtbRez.TabIndex = 4;
+            this.rtbRez.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 401);
+            this.Controls.Add(this.rtbRez);
+            this.Controls.Add(this.lblNrPainters);
+            this.Controls.Add(this.nupNrPainters);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Painter\'s Problem";
+            ((System.ComponentModel.ISupportInitialize)(this.nupNrPainters)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown nupNrPainters;
+        private System.Windows.Forms.Label lblNrPainters;
+        private System.Windows.Forms.RichTextBox rtbRez;
+    }
+}
+
---
+++ AA_Detyra1_v2/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AA_Detyra1_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<int> A;
+        int k;
+        int AvgWorkLoad;
+        Dictionary<int, int[]> Solution;
+        int[] Times;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            A = new List<int> { 100, 200, 300, 400, 500, 600, 700, 800, 900 };
+            //A = RandomArray();
+            k = (int)nupNrPainters.Value;
+            if (k > A.Count) k = A.Count;
+
+            AvgWorkLoad = CalculateAvgWork();
+            Solution = new Dictionary<int, int[]>();
+            Times = new int[k];
+
+            string strRez = "";
+            // String Formatting for Printing Input
+            strRez += "\nInputs:\nTables to paint are: {";
+            foreach (var item in A)
+                strRez += " " + item + " ";
+            strRez += " }\nNumber of painters is: " + k.ToString();
+
+            // Actual Partition Workload
+            WorkPartition(A);
+
+            // String Formatting for Printing Result
+            strRez += "\n\nResult:\nMinimum time to paint: " + Times.Max();
+            strRez += "\n\nSolution: ";
+            int l = 1;
+            foreach (var s in Solution)
+            {
+                strRez += "\nPainter" + l.ToString() + ":  ";
+                foreach (var sol in s.Value)
+                    strRez += " " + sol.ToString() + " ";
+                l++;
+            }
+            rtbRez.Text = strRez;
+        }
+        
+        private void WorkPartition(List<int> A)
+        {
+            if (k == 0 || A.Count == 0)
+                return;
+
+            int sum = 0, prevSum;
+            for (int j = 0; j < A.Count; j++)
+            {
+                prevSum = sum;
+                sum += A[j];
+
+                if (sum >= AvgWorkLoad)
+                {
+                    // prevSum ---d1--- avgWorkLoad ----d2--- sum
+                    int d1 = Math.Abs(AvgWorkLoad - prevSum);
+                    int d2 = Math.Abs(sum - AvgWorkLoad);
+                    if (d1 <= d2)
+                    {
+                        Solution.Add(k--, (int[])A.GetRange(0, j).ToArray());
+                        A.RemoveRange(0, j);
+                        Times[k] = prevSum;
+                    }
+                    else if (d2 < d1)
+                    {
+                        Solution.Add(k--, (int[])A.GetRange(0, j + 1).ToArray());
+                        A.RemoveRange(0, j + 1);
+                        Times[k] = sum;
+                    }
+                    WorkPartition(A);
+                    return;
+                }
+                else
+                {
+                    if (j + 1 == A.Count) // Last iteration
+                    {
+                        Solution.Add(k--, (int[])A.ToArray());
+                        Times[k] = sum;
+                        return;
+                    }
+                }
+            }
+        }
+
+        private int CalculateAvgWork()
+        {
+            int sum = 0;
+            for (int i = 0; i < A.Count; i++)
+                sum += A[i];
+            return (sum / k);
+        }
+
+        private List<int> RandomArray()
+        {
+            List<int> r = new List<int>();
+            Random rand = new Random(new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).Millisecond);
+            for (int i = 0; i < 10; i++)
+                r.Add(rand.Next(1, 9) * 10);
+            return r;
+        }
+    }
+}
